f35e8072a69d829bbcf74b3447743135e39e257f
HILLARY GOODNESS <goodek1991@gmail.com>
2022-06-24 22:42:01 +0100

    added alert

---
+++ nhc.js
@@ -0,0 +1,9 @@
+let myName = "Hillary Goodness" 
+let myHeight = "I am 5.5'ft"
+let myCountry = "I am from Nigeria"
+
+console.log(myName);
+console.log(myHeight);
+console.log(myCountry);
+
+alert("my name is Hillary Goodness, I am 5.5'ft in height, I am from Nigeria");
